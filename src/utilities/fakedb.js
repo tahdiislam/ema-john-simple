24b4fb88409f150863eb53fd8fcd4b72aa777205
tahdi-islam <tshdi60@gmail.com>
2022-09-26 18:19:53 +0600

    add product to localStorage

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,23 +1,25 @@
 // use local storage to manage cart data
 const addToDb = id =>{
+    // console.log('add db function')
     let shoppingCart = {};
 
-    //get the shopping cart from local storage
+    // get the shopping cart from local Storage
     const storedCart = localStorage.getItem('shopping-cart');
     if(storedCart){
-        shoppingCart = JSON.parse(storedCart);
+        shoppingCart = JSON.parse(storedCart)
     }
 
-    // add quantity
-    const quantity = shoppingCart[id];
+    // add quantity 
+    const quantity = shoppingCart[id]
     if(quantity){
-        const newQuantity = quantity + 1;
-        shoppingCart[id] = newQuantity;
-    }
-    else{
+        const newquantity = quantity + 1;
+        shoppingCart[id] = newquantity;
+    } else{
         shoppingCart[id] = 1;
     }
-    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart));
+    
+    // set to localStorage
+    localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
 }
 
 const removeFromDb = id =>{
