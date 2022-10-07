0e09bba32b9fbd1f4a124e2b6eaac15e9608eac3
tahdi-islam <tshdi60@gmail.com>
2022-10-07 22:35:58 +0600

    add link in navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 import Header from './Component/Header/Header';
+import Layout from './Component/Layout/Layout';
+import PageNotFound from './Component/PageNotFound/PageNotFound';
 import Shop from './Component/Shop/Shop';
 
 function App() {
+  const router = createBrowserRouter([
+    {path: '/', element:  <Layout/>, children: [
+      {path: '*', element: <PageNotFound/>},
+      {path: '/', element: <Shop/>},
+      {path: 'shop', element: <Shop/>},
+    ]}
+  ])
+
   return (
     <div className="App">
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router} />
+      {/* <Header></Header>
+      <Shop></Shop> */}
     </div>
   );
 }
---
+++ src/Component/PageNotFound/PageNotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PageNotFound.css'
+
+const PageNotFound = () => {
+    return (
+        <div className='page-not-found'>
+            <h1>404 | Page Not Found</h1>
+        </div>
+    );
+};
+
+export default PageNotFound;
