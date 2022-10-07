135f8b044f2d29f950bb8956bcb11422b34b01f5
tahdi-islam <tshdi60@gmail.com>
2022-10-08 01:53:31 +0600

    m

--- src/Component/Orders/Orders.js
+++ src/Component/Orders/Orders.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Cart from '../Cart/Cart';
+import ReviewItem from '../ReviewItem/ReviewItem';
 import './Orders.css'
 
 const Orders = () => {
     const { products, initialCart } = useLoaderData();
-    // console.log(products);
+    // console.log(initialCart);
+    const [ cart, setCart ] = useState(initialCart);
+    // console.log(cart);
+    // delete cart
+    const deleteShoppingCart = () => {
+        console.log('Hello World');
+    }
     return (
-        <div>
-            <h1>Order length: {products.length}</h1>
-            <h1>Cart length: {initialCart.length}</h1>
+      <div className="shop-container">
+        <div className="products-container">
+            {
+                cart.map(product => <ReviewItem key={product.id} product={product}/>)
+            }
         </div>
+        <div className="cart-container">
+          <Cart cart={cart} deleteShoppingCart={deleteShoppingCart}/>
+        </div>
+      </div>
     );
 };
 
---
+++ src/Component/ReviewItem/ReviewItem.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReviewItem = () => {
+    return (
+        <div>
+            <h1>This is Review Item</h1>
+        </div>
+    );
+};
+
+export default ReviewItem;
