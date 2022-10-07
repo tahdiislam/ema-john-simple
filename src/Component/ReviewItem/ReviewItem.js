46e44a7aa193a8e1abacde3860e2569c45b463b7
tahdi-islam <tshdi60@gmail.com>
2022-10-08 03:01:30 +0600

    add order product style

--- src/Component/ReviewItem/ReviewItem.js
+++ src/Component/ReviewItem/ReviewItem.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import './ReviewItem.css';
+import { Trash } from "react-bootstrap-icons";
 
-const ReviewItem = () => {
+const ReviewItem = ({product}) => {
+    const {img, name, price, quantity, shipping} = product
+    console.log(product);
     return (
-        <div>
-            <h1>This is Review Item</h1>
+      <div className="product-container">
+          <img src={img} alt="Product Images" />
+        <div className="product-info">
+          <p>{name}</p>
+          <p>
+            Price:
+            <span style={{ marginRight: "6px" }} className="price">
+              ${price}
+            </span>
+            Quantity: {quantity}
+          </p>
+          <p>
+            Shipping Charge: <span className="price">${shipping}</span>
+          </p>
         </div>
+        <div>
+          <button className='delete-btn'><Trash className='delete-icon'/></button>
+        </div>
+      </div>
     );
 };
 
